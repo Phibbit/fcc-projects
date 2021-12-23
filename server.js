66682cc1bdf130595ecd4e2c45f5ffe3c25aaf69
Philip Wonder <philip.wonder@gmail.com>
2021-12-23 12:57:28 -0500

    Created a main fcc project page
    Restructured server to allow multiple fcc projects to be hosted on the same site and created a new homepage.

--- server.js
+++ server.js
@@ -1,42 +1,23 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('main'));
+app.use("/Timestamp-Microservice", express.static('Timestamp-Microservice'));
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-app.get("/api/:date?", (req, res) => {
+app.get("/Timestamp-Microservice/api/:date?", (req, res) => {
   let input = req.params.date;
   let timestamp = parseInt(input);
-  let times = {unix: null, utc: null};
-  let e = {error: "Invalid Date"};
+  let times = { unix: null, utc: null };
+  let e = { error: "Invalid Date" };
 
   if (input == undefined) {
     times.unix = Date.parse(new Date());
   }
   else if (input == timestamp.toString() && new Date(timestamp) != e.error) {
     times.unix = timestamp;
-  } 
+  }
   else if (new Date(input) != e.error) {
     times.unix = Date.parse(input);
   }
@@ -49,7 +30,6 @@ app.get("/api/:date?", (req, res) => {
 });
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
